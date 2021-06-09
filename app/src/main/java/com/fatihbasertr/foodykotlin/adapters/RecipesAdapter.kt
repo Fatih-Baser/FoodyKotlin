9d367f7b2977196ec90fabdd083448b92324bf5e
fatih40 <baserfatih40@gmail.com>
2021-06-09 08:51:18 +0200

    continuous

---
+++ app/src/main/java/com/fatihbasertr/foodykotlin/adapters/RecipesAdapter.kt
@@ -0,0 +1,54 @@
+package com.fatihbasertr.foodykotlin.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.fatihbasertr.foodykotlin.databinding.RecipesRowLayoutBinding
+import com.fatihbasertr.foodykotlin.models.FoodRecipe
+import com.fatihbasertr.foodykotlin.models.Result
+import com.fatihbasertr.foodykotlin.util.RecipesDiffUtil
+
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+
+    private var recipes = emptyList<Result>()
+
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(result: Result){
+            binding.result = result
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentRecipe = recipes[position]
+        holder.bind(currentRecipe)
+    }
+
+    override fun getItemCount(): Int {
+        return recipes.size
+    }
+
+    fun setData(newData: FoodRecipe){// ne zaman internetten veri ceksin burdaki zamani alacagiz
+        val recipesDiffUtil =
+            RecipesDiffUtil(recipes, newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
+        recipes = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+}
